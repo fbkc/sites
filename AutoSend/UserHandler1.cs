29c33d9b13c173d868bbc34d9d653dec237f2a24
fbkc <a739650808@gmail.com>
2018-11-14 18:22:23 +0800

    33

--- AutoSend/UserHandler1.cs
+++ AutoSend/UserHandler1.cs
@@ -13,7 +13,7 @@ namespace AutoSend
     public class UserHandler1 : IHttpHandler, IRequiresSessionState
     {
         /// <summary>
-        /// 您将需要在网站的 Web.config 文件中配置此处理程序 
+        /// 您将需要在网站的 Web.config 文件中配置此处理程序
         /// 并向 IIS 注册它，然后才能使用它。有关详细信 息， 
         /// 请参阅以下链接: https://go.microsoft.com/?linkid=8101007
         /// </summary>
