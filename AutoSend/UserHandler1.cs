9830b16986738339593ddcd85a06263c7fed3f6c
fbkc <a739650808@gmail.com>
2018-11-14 18:28:23 +0800

    22

--- AutoSend/UserHandler1.cs
+++ AutoSend/UserHandler1.cs
@@ -14,7 +14,7 @@ namespace AutoSend
     {
         /// <summary>
         /// 您将需要在网站的 Web.config 文件中配置此处理程序
-        /// 并向 IIS 注册它，然后才能使用它。有关详细信 息， 
+        /// 并向 IIS 注册它，然后才能使用它。有关详细信息， 
         /// 请参阅以下链接: https://go.microsoft.com/?linkid=8101007
         /// </summary>
         #region IHttpHandler Members
